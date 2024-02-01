8cbce7641d426bac52778f084dd54e4f45a8d253
fedecarracedo <ftcarracedo@gmail.com>
2024-01-31 23:41:17 -0300

    Implementados los endpoints necesarios para la carga de unidades, lecciones y cursos

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.get('/:table/obtenerTodos', (req, res) => {
     con.query("SELECT * FROM " + req.params.table, (err, res, fields) => console.log(res))
 })
 
+app.get('/:table/obtenerSegunId/:id', (req, res) => {
+    con.query(`SELECT * FROM ${req.params.table} WHERE ${req.params.table}_id = ${req.params.id}`, (err, response, fields) => {
+        res.status(200).header('Access-Control-Allow-Origin', '*').send(response)
+    })
+})
+
 app.get('/usuario_curso/obtenerCursosUsuario/:usuario_id', (req, res) => {
     try {
         con.query("SELECT * FROM usuario_curso JOIN curso ON curso_id = usuario_curso_curso WHERE usuario_curso_usuario = " + req.params.usuario_id, (err, response, fields) => {
@@ -25,79 +31,35 @@ app.get('/usuario_curso/obtenerCursosUsuario/:usuario_id', (req, res) => {
     }
 })
 
-app.get('/leccion/obtenerLeccionesUnidad/:unidad_id', (req, res) => {
+app.get('/unidad/obtenerUnidadesCurso/:idCurso', (req, res) => {
     try {
-        con.query(`SELECT * FROM leccion WHERE leccion_unidad = ${req.params.unidad_id}`, (err, response, fields) => {
-            console.log(JSON.parse(JSON.stringify(response))[0])
+        con.query(`SELECT * FROM unidad WHERE unidad_curso = ${req.params.idCurso}`, (err, response, fields) => {
             res.status(200).header('Access-Control-Allow-Origin', '*').send(response)
         })
     } catch (error) {
-        throw(error)
+        res.status(501)
+        throw error
     }
 })
 
-const JSONData = { "elementos": [
-    {
-        "id": "oUq2g_tl8y",
-        "type": "header",
-        "data": {
-           "text": "Editor.js",
-           "level": 2
-        }
-     },
-     {
-        "id": "zbGZFPM-iI",
-        "type": "paragraph",
-        "data": {
-           "text": "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration."
-        }
-     },
-     {
-        "id": "qYIGsjS5rt",
-        "type": "header",
-        "data": {
-           "text": "Key features",
-           "level": 3
-        }
-     },
-     {
-        "id": "XV87kJS_H1",
-        "type": "list",
-        "data": {
-           "style": "unordered",
-           "items": [
-              "It is a block-styled editor",
-              "It returns clean data output in JSON",
-              "Designed to be extendable and pluggable with a simple API"
-           ]
-        }
-     },
-     {
-        "id": "AOulAjL8XM",
-        "type": "header",
-        "data": {
-           "text": "What does it mean «block-styled editor»",
-           "level": 2
-        }
-     },
-     {
-        "id": "cyZjplMOZ0",
-        "type": "paragraph",
-        "data": {
-           "text": "Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js"
-        }
-     }
-]}
+app.get('/leccion/obtenerLeccionesUnidad/:idUnidad', (req, res) => {
+    try {
+        con.query(`SELECT * FROM leccion WHERE leccion_unidad = ${req.params.idUnidad}`, (err, response, fields) => {
+            res.status(200).header('Access-Control-Allow-Origin', '*').send(response)
+        })
+    } catch (error) {
+        res.status(501)
+        throw error
+    }
+}) 
 
-app.get('/', (req, res) => {
+app.post('/leccion/crearLeccion', (req, res) => {
     con.query(`INSERT INTO leccion (leccion_unidad,leccion_nombre,leccion_contenido) VALUES ('1','Negociacion y oratoria','${JSON.stringify(JSONData.elementos)}')`, (err, response, fields) => {
-        console.log(response)
-        console.log(err)
         res.status(200).header('Access-Control-Allow-Origin', '*').send(response)
     })
     console.log("Solicitud enviada")
 })
 
-app.listen('8080', function () {
+app.listen('8080', () => {
     console.log("Servidor escuchando en el puerto 8080.");
 });
